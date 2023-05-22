984cb18e1145c9edaf50a1345a4ed8305807509a
Camilla Damgaard Jensen <cami45h9@stud.kea.dk>
2023-05-22 11:23:34 +0200

    forhandlere

---
+++ wp-content/themes/loeve_childtheme/page-forhandlere.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<h1>find forhandlere</h1>
+				<article>
+			<h2>Sjæl Aarhus</h2>
+			<p>Klostergade 26 <br>
+				8000 Aarhus <br>
+				sjael-aarhus.dk <br>
+				+45 30 60 90 02</p>
+			</article>
+
+				<article>
+			<h2>Helle Thorup</h2>
+			<p>Frederiksgade 1, 2. sal
+				<br>
+				8000 Aarhus<br>
+				hellethorup.dk <br>
+				+45 86 18 39 00</p>
+			</article>
+
+			<article>
+			<h2>RAASTED</h2>
+			<p>Badstuegade 1E
+				<br>
+				8000 Aarhus<br>
+				raasted.dk <br>
+				+45 40 78 55 98</p>
+			</article>
+
+			<article>
+			<h2>Beauty Avenue</h2>
+			<p>Allegade 24A, 1. sal
+				<br>
+				1850 Frederiksberg<br>
+				beautyavenue.dk <br>
+				+45 33 36 20 90</p>
+			</article>
+
+				<article>
+			<h2>Pure Senses</h2>
+			<p>Århusgade 88, 4
+				<br>
+				2100 København<br>
+				puresenses.dk <br>
+				+45 26 37 20 26</p>
+			</article>
+		
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
