2fb00766af09efccc07df8cebffef4c8237b869c
Camilla Damgaard Jensen <cami45h9@stud.kea.dk>
2023-05-22 10:03:38 +0200

    grid historien bag

--- wp-content/themes/loeve_childtheme/page-historien-bag.php
+++ wp-content/themes/loeve_childtheme/page-historien-bag.php
@@ -15,22 +15,13 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			tester123
-
-			<?php
-
-			// Start the Loop.
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
-			endwhile; // End the loop.
-			?>
+            <h1>historien bag</h1>
+			<article>
+				<img src="" alt="stine">
+				<h2>min vision</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+			</article>
+			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
